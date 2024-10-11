653c1336b77a6cc5f214d8026f6644e8b9a512f5
drondistortion <cancelbutton@gmail.com>
2024-10-11 13:10:59 +0300

    fuzziness

--- fzcl.go
+++ fzcl.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
+var FUZZ = 2
+
+var FUZZINESS = map[int]time.Duration{
+	1: 5,
+	2: 15,
+	3: 30,
+	4: 60, // every hour
+	5: 60, // morning, day, evening, night
+}
+
 var WORDS = map[int]string{
+	0:  "midnight",
 	1:  "one",
 	2:  "two",
 	3:  "three",
@@ -21,6 +33,7 @@ var WORDS = map[int]string{
 	15: "quarter",
 	20: "twenty",
 	30: "half",
+	45: "quarter",
 }
 
 func hourWord(hours int) string {
@@ -31,33 +44,50 @@ func hourWord(hours int) string {
 	return WORDS[hours]
 }
 
-func fuzzy(now time.Time) string {
-	hours := now.Hour()
-	minutes := now.Minute()
+func fuzzy(now time.Time, fuzz int) string {
+	fuzziness, present := FUZZINESS[fuzz]
+	if !present {
+		return fmt.Sprintf("%s", "sometime somewhere")
+	}
+
+	t := now.Round(fuzziness * time.Minute)
+
+	hours := t.Hour()
+	minutes := t.Minute()
+
 	glue := "past"
 
-	rounded := (minutes + 2) % 60 / 5 * 5
-	if rounded > 35 {
-		rounded = 60 - rounded
-		hours++
-		glue = "til"
+	if minutes > 35 {
+		glue = "to"
+		hours = t.Round(time.Hour).Hour()
 	}
 
-	if rounded == 25 || rounded == 35 {
-		rounded = 30
-	}
-
-	if rounded == 0 {
-		if minutes > 30 {
-			hours++
+	if minutes == 0 {
+		if (hours != 0) {
+			return fmt.Sprintf("%s o'clock", hourWord(hours))
+		} else {
+			return fmt.Sprintf("%s", hourWord(hours))
 		}
-
-		return fmt.Sprintf("%s o'clock", hourWord(hours))
 	} else {
-		return fmt.Sprintf("%s %s %s", WORDS[rounded], glue, hourWord(hours))
+		return fmt.Sprintf("%s %s %s", WORDS[minutes], glue, hourWord(hours))
 	}
 }
 
+var fuzzy_func = []func(time.Time) string {
+	abit_fuzzy,
+	fuzzy,
+	more_fuzzy,
+	vague,
+	abmiguous,
+}
+
 func main() {
-	fmt.Println(fuzzy(time.Now()))
+	f := flag.Int(
+		"f", 
+		FUZZ,
+		"fuzziness degree: 1 - a bit fuzzy, 2 - fuzzy, 3 - more fuzzy, 4 - vague, 5 - abmiguous")
+	flag.Parse();
+
+	//fmt.Println(fuzzy(time.Date(2024, 6, 6, 23, 40, 0, 0, time.UTC), *f))
+	fmt.Println(fuzzy(time.Now(), *f))
 }
--- fzcl_test.go
+++ fzcl_test.go
@@ -18,16 +18,22 @@ func (p Parameters) Name() string {
 
 func TestTime(t *testing.T) {
 	var times = []Parameters{
+		{0, 0, "midnight"},
+		{23, 40, "quarter to midnight"},
+		{0, 20, "quarter past midnight"},
 		{12, 0, "twelve o'clock"},
 		{12, 2, "twelve o'clock"},
+		{5, 40, "quarter to six"},
 		{11, 58, "twelve o'clock"},
-		{3, 22, "twenty past three"},
+		{3, 22, "quarter past three"},
 		{3, 37, "half past three"},
+		{1, 29, "half past one"},
+		{1, 20, "quarter past one"},
 	}
 	for _, param := range times {
 		t.Run(param.Name(), func(t *testing.T) {
 			now, _ := time.Parse("3:04", param.Name())
-			result := fuzzy(now)
+			result := fuzzy(now, 2)
 			if result != param.out {
 				t.Errorf("got %s, want %s", result, param.out)
 			}
